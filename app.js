f9a016f8c73317275ba5858ab95b13d7f1db0795
YJ0067 <yashjadhav0067@gmail.com>
2025-03-19 10:44:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+('serviceWorker' in Navigator)
+{
+    navigator.serviceWorker.register('/sw.js')
+    .then((reg) => console.log('Service Worker registered',reg))
+    .catch((err) => console.log('Service Worker not registered',err));
+
+}
+
